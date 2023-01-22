5b773d13c3cc8965a6f18cdfc302b7ac33ca71dd
Jared Tichacek <jared.tichacek@gmail.com>
2023-01-21 21:12:45 -0600

    added search history through local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,56 @@
 // global DOM variables
 let cityInput = document.getElementById('city-input')
 let searchBtn = document.getElementById('search-btn')
+let searchHistory = []
 
 // event listener when user enters city and clicks search button
 searchBtn.addEventListener('click', function () {
     // grabs user input from city text field
     let city = cityInput.value
+
+    // displays search history found in local storage
+    function showSearchHistory () {
+        // adds latest search to search history array
+        searchHistory.push(city)
+        // save to local storage
+        localStorage.setItem('search', JSON.stringify(searchHistory))
+    
+        // grabs previous searches from local storage
+    let storedHistory = JSON.parse(localStorage.getItem('search'))
+    if(storedHistory !== null) {
+        storedHistory = searchHistory
+    }
+    
+    // container element that includes buttons for each previous search
+    let searchHistoryContainer = document.getElementById('search-history')
+    
+    // generate buttons for each previous search
+    for (let i = 0; i < searchHistory.length; i++) {
+        const historyList = searchHistory[i];
+        const historyBtn = document.createElement('button')
+        historyBtn.setAttribute( 'class', 'btn btn primary')
+        historyBtn.textContent = historyList
+        searchHistoryContainer.appendChild(historyBtn)
+    }
+    
+    }
     // searchWeather function will only run if value is found in city text field
     if(city === "") {
         return
-    } else
-    searchWeather(city)
+    } else {
+        searchWeather(city)
+        showSearchHistory()
+        // addSearchHistory()
+    }
 })
+// adds search to local storage
+// function addSearchHistory () {
+// }
+
+// // function that grabs previous searches from local storage
+// function pullSearchHistory () {
+    // }
+    
 // function that displays current weather and forecast fetched from OpenWeather API
 function searchWeather(city) {
     let weatherApiKey = '542bb7e6d08fd71cf01b529cf638c811'
@@ -19,36 +58,44 @@ function searchWeather(city) {
     let weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weatherApiKey}&units=imperial`
     fetch(weatherUrl)
     .then(function (res) {
+        console.log(res)
         return res.json()
     })
     .then(function (data) {
         console.log(data)
         // container element that will include all current weather info
         let currentWeatherContainer = document.getElementById('current-weather')
+
         // city information to be displayed
         let cityEl = document.createElement('span')
         cityEl.innerText = data.name
         currentWeatherContainer.appendChild(cityEl)
+
         // current date to be displayed
         let dateEl = document.createElement('span')
         dateEl.innerText = dayjs(data.dt_txt).format('dddd, MMM D, YYYY')
         currentWeatherContainer.appendChild(dateEl)
+
         // icon representing weather condition to be displayed
         let iconEl = document.createElement('img')
         iconEl.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`
         currentWeatherContainer.appendChild(iconEl)
+
         // current temperature to be displayed
         let tempEl = document.createElement('p')
         tempEl.innerText = 'Temperature: ' + data.main.temp + '°F'
         currentWeatherContainer.appendChild(tempEl)
+
         // current humidity to be displayed
         let humidityEl = document.createElement('p')
         humidityEl.innerText = 'Humidity: ' + data.main.humidity + '%'
         currentWeatherContainer.appendChild(humidityEl)
         // current wind speed to be displayed
+
         let windSpeedEl = document.createElement('p')
         windSpeedEl.innerText = 'Wind Speed: ' + data.wind.speed + ' MPH'
         currentWeatherContainer.appendChild(windSpeedEl)
+        
         // 5-day forecast to be rendered to the container
         let futureWeatherUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${weatherApiKey}&units=imperial`
         fetch(futureWeatherUrl)
@@ -63,22 +110,27 @@ function searchWeather(city) {
             for (let i = 0; i < data.list.length; i += 8) {
                 const forecast = data.list[i];
                 console.log(forecast)
+
                 // iterates over future date
                 let futureDateEl = document.createElement('span')
                 futureDateEl.innerText = dayjs(forecast.dt_txt).format('dddd, MMM D, YYYY')
                 futureWeatherContainer.appendChild(futureDateEl)
+
                 // iterates over future weather condition icon
                 let futureIconEl = document.createElement('img')
                 futureIconEl.src = `http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png`
                 futureWeatherContainer.appendChild(futureIconEl)
+
                 // iterates over future temperature
                 let futureTempEl = document.createElement('p')
                 futureTempEl.innerText = 'Temperature: ' + forecast.main.temp + '°F'
                 futureWeatherContainer.appendChild(futureTempEl)
+
                 // iterates over future humidity
                 let futureHumidityEl = document.createElement('p')
                 futureHumidityEl.innerText = 'Humidity: ' + forecast.main.humidity + '%'
                 futureWeatherContainer.appendChild(futureHumidityEl)
+
                 // iterates over future wind speed
                 let futureWindSpeedEl = document.createElement('p')
                 futureWindSpeedEl.innerText = 'Wind Speed: ' + forecast.wind.speed + ' MPH'
@@ -87,4 +139,5 @@ function searchWeather(city) {
             }
         })
     })
+    // showSearchHistory()
 }
